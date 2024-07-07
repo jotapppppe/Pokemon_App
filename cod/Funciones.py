44a2953d17603b2c2265d3f0f622d651b2c3f7a4
Juan Pablo Morgan <juanpablomorgans@gmail.com>
2024-07-06 19:38:53 -0600

    fix: se sube funciones.py con las funciones documentadas

--- cod/Funciones.py
+++ cod/Funciones.py
@@ -15,6 +15,20 @@ tipo = tipo.pivot_table(index=None, columns='Tipo', values='Numero', aggfunc='fi
 
 # FUNCIONES
 def comparacion(pkmn1, pkmn2, pkdx):
+    '''
+    Esta función calcula la puntuación entre dos Pokémon en función de sus tipos.
+
+    Parameters:
+    pkmn1 (int): El índice del primer Pokémon en el Pokédex DataFrame.
+    pkmn2 (int): El índice del segundo Pokémon en el Pokédex DataFrame.
+    pkdx (DataFrame): el DataFrame de Pokédex que contiene información sobre Pokémon.
+
+    Returns:
+    int: La puntuación calculada entre los dos Pokémon.
+
+    Note:
+    La función utiliza el DataFrame 'tabla_de_tipos' para determinar la efectividad de cada tipo frente a otro.
+    '''
 
     tipo_pkmn1_1 = pkdx.loc[pkmn1, 'Type1']
     tipo11 = tipo.loc['Numero', tipo_pkmn1_1]
@@ -52,6 +66,20 @@ def comparacion(pkmn1, pkmn2, pkdx):
 
 
 def puntaje_total(pkmn1, pkmn2, pkdx):
+    '''
+    Calcula la puntuación total entre dos Pokémon en función de sus tipos.
+
+    Parameters:
+    pkmn1 (int): El índice del primer Pokémon en el Pokédex DataFrame.
+    pkmn2 (int): El índice del segundo Pokémon en el Pokédex DataFrame.
+    pkdx (DataFrame): El DataFrame de Pokédex que contiene información sobre Pokémon.
+
+    Returns:
+    int: La puntuación total calculada entre los dos Pokémon.
+
+    Note:
+    La puntuación se calcula restando la puntuación del segundo Pokémon de la puntuación del primer Pokémon.
+    '''
     valor_1 = comparacion(pkmn1, pkmn2, pkdx)
     valor_2 = comparacion(pkmn2, pkmn1, pkdx)
 
@@ -60,6 +88,22 @@ def puntaje_total(pkmn1, pkmn2, pkdx):
 
 
 def starter(starters, gym, pkdx):
+    '''
+    Calcula la puntuación total de cada Pokémon inicial en función de sus interacciones con los Pokémon del gimnasio.
+
+    Parameters:
+    starters (lista): una lista de tres números enteros que representan los índices del Pokémon inicial en el marco de datos.
+    gym (dict): Un diccionario donde las claves son los nombres de los líderes de los gimnasios y los valores son listas de índices de Pokémon en cada gimnasio.
+    pkdx (DataFrame): un DataFrame que contiene datos de Pokémon, incluidos sus tipos y estadísticas.
+
+    Devoluciones:
+    DataFrame: Un DataFrame con dos columnas: 'Tipo' (que representa los tipos de Pokémon iniciales) y 'Puntaje' (que representa la puntuación total de cada Pokémon inicial).
+
+    Nota:
+    La función calcula la puntuación total de cada Pokémon inicial iterando sobre los Pokémon del gimnasio y llamando a la función puntaje_total.
+    La función puntaje_total calcula la puntuación entre dos Pokémon en función de sus tipos.
+    La puntuación se calcula restando la puntuación del segundo Pokémon de la puntuación del primer Pokémon.
+    '''
     total_1 = 0
     total_2 = 0
     total_3 = 0
@@ -87,7 +131,22 @@ def starter(starters, gym, pkdx):
     return resultado
 
 def general(pkmn, gym, pkdx):
+    '''
+    Calcula la puntuación total de un Pokémon específico contra todos los Pokémon del gimnasio.
 
+    Parámetros:
+    pkmn (int): El índice del Pokémon específico en el DataFrame de Pokédex.
+    gym (dict): Un diccionario donde las claves son los nombres de los líderes de los gimnasios y los valores son listas de índices de Pokémon en cada gimnasio.
+    pkdx (DataFrame): un DataFrame que contiene datos de Pokémon, incluidos sus tipos y estadísticas.
+
+    Devoluciones:
+    int: La puntuación total calculada para el Pokémon específico contra todos los Pokémon en el gimnasio.
+
+    Nota:
+    La función calcula la puntuación iterando sobre los Pokémon en el gimnasio y llamando a la función puntaje_total.
+    La función puntaje_total calcula la puntuación entre dos Pokémon en función de sus tipos.
+    La puntuación se calcula restando la puntuación del segundo Pokémon de la puntuación del primer Pokémon.
+    '''
     total = 0
     
     for lider in range(len(gym)):
@@ -97,6 +156,22 @@ def general(pkmn, gym, pkdx):
     return total
 
 def Top_regional (gym, pkdx, pkdx_nacional):
+    '''
+    Esta función calcula la puntuación total de cada Pokémon en una Pokédex regional determinada frente a todos los Pokémon en un gimnasio.
+    Filtra Pokémon con un total de estadísticas base (Estadísticas) superior a 480.
+    La función devuelve un DataFrame ordenado por la puntuación total en orden descendente.
+
+    Parámetros:
+    gym (dict): un diccionario donde las claves son los nombres de los líderes de los gimnasios y los valores son listas de índices de Pokémon en cada gimnasio.
+    pkdx (DataFrame): un DataFrame que contiene datos de Pokémon, incluidos sus tipos y totales de estadísticas base.
+    pkdx_nacional (DataFrame): un DataFrame que contiene datos nacionales de Pokédex, utilizado para calcular puntuaciones.
+
+    Devoluciones:
+    DataFrame: Un DataFrame con las columnas 'Número de Pokédex', 'Puntaje', 'Stats', 'Tipo_1', 'Tipo_2'.
+    El DataFrame está ordenado por 'Puntaje' en orden descendente y 'Estadísticas' en orden descendente para Pokémon con la misma puntuación.
+ """
+    '''
+
     # Lista para almacenar los resultados
     resultados = []
     
